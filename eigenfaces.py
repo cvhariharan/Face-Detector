f9bef05a345333c768dd61fe528c2b1311659a25
Hariharan <theroboticlabs@gmail.com>
2018-04-28 22:12:46 +0530

    Shows the average face

--- eigenfaces.py
+++ eigenfaces.py
@@ -1,13 +1,15 @@
 from PIL import Image
 import numpy as np
-import os
+from numpy import linalg as LA
+import os, random
 
-training_size = 20
+training_size = 30
 img = Image.open("faces/subject01.centerlight")
 width = img.size[0]
 height = img.size[1]
 
 all_img_names = os.listdir("faces/")
+#random.shuffle(all_img_names)
 num_imgs = len(all_img_names)
 
 #Vector to hold the average pixel values of the entire training set
@@ -19,18 +21,31 @@ i=0
 for each_img in all_img_names:
     img = Image.open("faces/"+each_img)
     pixels = np.asarray(img)
+    
+    #print(pixels)
     pixels = np.reshape(pixels,(width*height,1))
+    #print(pixels)
     total_img_pixels = pixels + total_img_pixels
     pixel_matrix = np.append(pixel_matrix,pixels,axis=1)
     i = i+1
     if i==training_size:
         break
-
 #Delete the first column of zeros
 pixel_matrix = np.delete(pixel_matrix,0,1)
 total_img_pixels = total_img_pixels / num_imgs
-#print(pixel_matrix)
+temp = np.reshape(total_img_pixels,(height,width))
+im = Image.fromarray(temp)
+im.show()
+print(pixel_matrix.shape)
 error_matrix = pixel_matrix - total_img_pixels
 #print(error_matrix)
 L = np.matmul(pixel_matrix.T,pixel_matrix)
-#print(L)
+L = L/training_size
+print(L.shape)
+
+#Calculate eigenvalues and eigenvectors
+eigenvalues, eigenvectors = LA.eig(L)
+#print(eigenvectors)
+
+eigenfaces = np.matmul(pixel_matrix,eigenvectors)
+#print(eigenfaces)
