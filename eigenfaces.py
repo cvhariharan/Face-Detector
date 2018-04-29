4a2b8e50347a4ae3daf1f7cf5838b7dd08f1b766
Hariharan <theroboticlabs@gmail.com>
2018-04-29 17:55:13 +0530

    Working Version

--- eigenfaces.py
+++ eigenfaces.py
@@ -3,85 +3,89 @@ import numpy as np
 from numpy import linalg as LA
 import os, random
 
-training_size = 30
-img = Image.open("faces/subject01.centerlight")
-width = img.size[0]
-height = img.size[1]
+class FaceDectector:
+    def __init__(self, training_size, sample, directory):
+        self.training_size = training_size
+        self.directory = directory
+        img = Image.open(sample)
+        self.width = img.size[0]
+        self.height = img.size[1]
+    
+    def train(self):
+        self.all_img_names = os.listdir(self.directory)
+        num_imgs = len(self.all_img_names)
 
-all_img_names = os.listdir("faces/")
-#all_img_names = all_img_names[10:]
-#random.shuffle(all_img_names)
-num_imgs = len(all_img_names)
+        #Vector to hold the average pixel values of the entire training set
+        total_img_pixels = np.zeros((self.width*self.height,1))
 
-#Vector to hold the average pixel values of the entire training set
-total_img_pixels = np.zeros((width*height,1))
+        pixel_matrix = [] 
+        i=0
+        for each_img in self.all_img_names:
+            img = Image.open(self.directory+each_img)
+            pixels = np.asarray(img)
+            pixels = np.reshape(pixels,(self.width*self.height,1))
+            total_img_pixels = pixels + total_img_pixels
+            pixel_matrix.append(pixels)
+            i = i+1
+            if i == self.training_size:
+                break
 
-pixel_matrix = [] #np.zeros((width*height,1))
-i=0
-for each_img in all_img_names:
-    img = Image.open("faces/"+each_img)
-    pixels = np.asarray(img)
-    pixels = np.reshape(pixels,(width*height,1))
-    total_img_pixels = pixels + total_img_pixels
-    pixel_matrix.append(pixels)
-    i = i+1
-    if i==training_size:
-        break
+        pixel_matrix = np.reshape(np.array(pixel_matrix).T,(self.width*self.height,self.training_size))
+        print(pixel_matrix.shape)
+        total_img_pixels = total_img_pixels / self.training_size
+        self.total_img_pixels = total_img_pixels
+        temp = np.reshape(total_img_pixels,(self.height,self.width))
 
-#Delete the first column of zeros
-pixel_matrix = np.reshape(np.array(pixel_matrix).T,(width*height,training_size))#np.delete(pixel_matrix,0,1)
-print(pixel_matrix.shape)
-total_img_pixels = total_img_pixels / training_size
-temp = np.reshape(total_img_pixels,(height,width))
-#im = Image.fromarray(temp)
-#im.show()
-#print(pixel_matrix)
-#print("T")
-error_matrix = pixel_matrix - total_img_pixels
-#print(error_matrix)
-L = np.matmul(error_matrix.T,error_matrix)
-L = L / training_size
-#print(error_matrix.shape)
+        #Shows the average image
+        #im = Image.fromarray(temp)
+        #im.show()
 
-#Calculate eigenvalues and eigenvectors
-eigenvalues, eigenvectors = LA.eigh(L)
+        self.error_matrix = pixel_matrix - total_img_pixels
+        L = np.matmul((self.error_matrix).T,self.error_matrix)
+        L = L / self.training_size
 
-#Take only top 20 eigenvectors with highest eigenvalues
-sort_indices = eigenvalues.argsort()[::-1]
-sort_indices = sort_indices[:20]
-eigenvectors = eigenvectors[:,sort_indices]
-eigenfaces = []
-for i in range(eigenvectors.shape[1]):
-    eigenfaces.append(np.matmul(error_matrix,eigenvectors[:,i]))
-eigenfaces = np.array(eigenfaces).T
-#print(eigenfaces.shape)
+        #Calculate eigenvalues and eigenvectors
+        eigenvalues, eigenvectors = LA.eigh(L)
 
-#Contribution of the training set
-contribution_matrix = []
-for i in range(error_matrix.shape[1]):
-    contribution_matrix.append(np.matmul(eigenfaces.T,error_matrix[:,i]))
+        #Take only top 20 eigenvectors with highest eigenvalues
+        sort_indices = eigenvalues.argsort()[::-1]
+        sort_indices = sort_indices[:20]
+        eigenvectors = eigenvectors[:,sort_indices]
+        self.eigenfaces = []
+        for i in range(eigenvectors.shape[1]):
+            self.eigenfaces.append(np.matmul(self.error_matrix,eigenvectors[:,i]))
+        
+        self.eigenfaces = np.array(self.eigenfaces).T
+         #Contribution of the training set
+        self.contribution_matrix = []
+        for i in range((self.error_matrix).shape[1]):
+            (self.contribution_matrix).append(np.matmul((self.eigenfaces).T,self.error_matrix[:,i]))
 
-#contribution_matrix = np.delete(contribution_matrix,0,1)
-contribution_matrix = np.array(contribution_matrix).T
-#print(contribution_matrix)
-#print(all_img_names[np.argmax(LA.norm(contribution_matrix))])
-#Classify
-test = Image.open("test/subject01.glasses")
-test_matrix = np.asarray(test)
-test_matrix = np.reshape(test_matrix,(width*height,1))
-test_error_matrix = test_matrix - total_img_pixels
+        self.contribution_matrix = np.array(self.contribution_matrix).T
 
-omega = np.matmul(eigenfaces.T,test_error_matrix)
+    def detect(self,filename):
+        #Classify
+        test = Image.open(filename)
+        test_matrix = np.asarray(test)
+        test_matrix = np.reshape(test_matrix,(self.width*self.height,1))
+        test_error_matrix = test_matrix - self.total_img_pixels
 
-score = contribution_matrix - omega
-all_scores = []
-for j in range(score.shape[1]):
-    all_scores.append(LA.norm(score[:,j]))
+        omega = np.matmul((self.eigenfaces).T,test_error_matrix)
 
-#print(all_scores)
-n_scores = np.array(all_scores)
+        score = self.contribution_matrix - omega
+        all_scores = []
+        for j in range(score.shape[1]):
+            all_scores.append(LA.norm(score[:,j]))
 
-print("Detected Image: faces/"+all_img_names[np.argmin(n_scores)])
+        #print(all_scores)
+        n_scores = np.array(all_scores)
 
-detected = Image.open("faces/"+all_img_names[np.argmin(n_scores)])
-detected.show()
+        print("Detected Image: "+self.directory+" "+self.all_img_names[np.argmin(n_scores)])
+
+        detected = Image.open(self.directory+self.all_img_names[np.argmin(n_scores)])
+        detected.show()
+
+#Example
+d = FaceDectector(30,"faces/subject01.centerlight","faces/")
+d.train()
+d.detect("test/subject02.glasses")
