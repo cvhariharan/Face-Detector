1a8be3c0ed3dcff5931394999c1ce77ad3eeb257
Hariharan <theroboticlabs@gmail.com>
2018-04-29 12:07:03 +0530

    Calculates eigenfaces

--- eigenfaces.py
+++ eigenfaces.py
@@ -16,24 +16,20 @@ num_imgs = len(all_img_names)
 total_img_pixels = np.zeros((width*height,1))
 
 pixel_matrix = np.zeros((width*height,1))
-
 i=0
 for each_img in all_img_names:
     img = Image.open("faces/"+each_img)
     pixels = np.asarray(img)
-    
-    #print(pixels)
     pixels = np.reshape(pixels,(width*height,1))
-    #print(pixels)
     total_img_pixels = pixels + total_img_pixels
     pixel_matrix = np.append(pixel_matrix,pixels,axis=1)
     i = i+1
     if i==training_size:
         break
+
 #Delete the first column of zeros
 pixel_matrix = np.delete(pixel_matrix,0,1)
 total_img_pixels = total_img_pixels / training_size
-print(total_img_pixels)
 temp = np.reshape(total_img_pixels,(height,width))
 im = Image.fromarray(temp)
 im.show()
@@ -42,12 +38,18 @@ im.show()
 error_matrix = pixel_matrix - total_img_pixels
 #print(error_matrix)
 L = np.matmul(error_matrix.T,error_matrix)
-L = L/training_size
-print(L.shape)
+L = L / training_size
+print(error_matrix.shape)
 
 #Calculate eigenvalues and eigenvectors
-eigenvalues, eigenvectors = LA.eig(L)
-print(eigenvectors)
+eigenvalues, eigenvectors = LA.eigh(L)
+#Take only top 20 eigenvectors with highest eigenvalues
+sort_indices = eigenvalues.argsort()[::-1]
+sort_indices = sort_indices[:20]
+eigenvectors = eigenvectors[:,sort_indices]
+#print(eigenvectors)
+#print(np.argsort(eigenvalues))
+
 
 eigenfaces = np.matmul(error_matrix,eigenvectors)
-#print(eigenfaces)
+print(eigenfaces)
